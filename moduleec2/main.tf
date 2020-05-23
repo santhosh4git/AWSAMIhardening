bd55c42ba99c1c5ec4e1f380df296d85e65a79a1
Santhosh Surya (Infosys) <S.Surya@ucas.ac.uk>
2020-05-23 17:54:03 +0100

    added

---
+++ moduleec2/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region     = "${var.region}"
+  profile    = "${var.myprofile}"
+}
+
+resource "aws_instance" "my_ec2_server" {
+  ami           = "${var.ami_id}"
+  instance_type = "${var.instance_type}"
+  tags = {
+    Name = "${var.tag}"
+  }
+}
